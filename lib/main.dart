d34ee6797f6c3b4d297e83e745a0fa2e830cd244
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-12-16 13:56:09 +0900

    環境変数の設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'my_app.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+      url: dotenv.get('SUPABASE_URL'), anonKey: dotenv.get('SUPABASE_KEY'));
   runApp(const MyApp());
 }
